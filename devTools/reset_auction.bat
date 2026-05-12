978d44b5732575be2cdddf18c10a28fd958b2abc
ian9494 <ian20050904@gmail.com>
2026-05-12 15:34:52 +0800

    20260511-1
    預留一些UI問題

--- devTools/reset_auction.bat
+++ devTools/reset_auction.bat
@@ -5,4 +5,7 @@ if "%TOKEN%"=="" set "TOKEN=dev-token"
 curl -X POST http://localhost:3000/api/admin/reset-auctions ^
   -H "x-admin-token: %TOKEN%"
 
+curl -X POST https://auction.noctration.dev/api/admin/reset-auctions ^
+  -H "x-admin-token: %TOKEN%"
+
 pause
